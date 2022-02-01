69b00bdf71224e57e5fade8d74de789ebb3ac939
Alex Bortok <dev@bortok.com>
2022-02-01 14:58:12 -0800

    removed "-v" from "go test"

--- kne-demo-tests/clos-4node-pod/test_ecmp_failover.sh
+++ kne-demo-tests/clos-4node-pod/test_ecmp_failover.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-go test -run=TestClosPodHostsPacketForwardBgpV4_V4Flows -v
+go test -run=TestClosPodHostsPacketForwardBgpV4_V4Flows
 
 echo "Next is to shutdown TOR links to POD1-1 and check if traffic would still flow."
 read -p "Press Enter to continue..."
 
 go test -run=TestClosPodHosts_ShutEth1 -v
-go test -run=TestClosPodHostsPacketForwardBgpV4_V4Flows -v
+go test -run=TestClosPodHostsPacketForwardBgpV4_V4Flows
 
 echo "Next is to bring TOR links to POD1-1 back up and check if traffic would still flow."
 read -p "Press Enter to continue..."
 
 go test -run=TestClosPodHosts_NoShutEth1 -v
-go test -run=TestClosPodHostsPacketForwardBgpV4_V4Flows -v
+go test -run=TestClosPodHostsPacketForwardBgpV4_V4Flows
